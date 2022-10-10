fc34d2c6cdba7bb30b7fe219c6589926e0937d30
ChristianSB <christiansb.kodehode@gmail.com>
2022-10-10 13:10:36 +0200

    test

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,13 @@
 import Navbar from "../components/Navbar";
 
 export const LandingPage = () => {
-    return (
-        <>
-            <Navbar />
-            <h1>Landing Page</h1>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <h1>Landing Page</h1>
+      <p>Test</p>
+    </>
+  );
 };
 
 export default LandingPage;
