0582650517713d025b6b19839297357c17ec953a
Ram <ram.kodehode@gmail.com>
2022-10-12 12:29:37 +0200

    removed div

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import LandingPage from "./pages/LandingPage";
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/landingPage/HomePage";
-import ContactPage from "./pages/landingPage/ContactPage";
-import PlanetPage from "./pages/landingPage/PlanetPage";
-import AboutPage from "./pages/landingPage/AboutPage";
+import LandingPage from './pages/LandingPage';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/landingPage/HomePage';
+import ContactPage from './pages/landingPage/ContactPage';
+import PlanetPage from './pages/landingPage/PlanetPage';
+import AboutPage from './pages/landingPage/AboutPage';
 
 function App() {
-
-  
-    return (
-        <div>
-            <LandingPage />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/AboutPage" element={<AboutPage />} />
-                <Route path="/ContactPage" element={<ContactPage />} />
-                <Route path="/PlanetPage" element={<PlanetPage />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <>
+      <LandingPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/AboutPage" element={<AboutPage />} />
+        <Route path="/PlanetPage" element={<PlanetPage />} />
+        <Route path="/ContactPage" element={<ContactPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
