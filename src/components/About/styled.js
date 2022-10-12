22db19f91d2e442cd03bd508d9d83f7edc400342
Ram <ram.kodehode@gmail.com>
2022-10-12 16:15:48 +0200

    Added styledcomponents

---
+++ src/components/About/styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  align-items: center;
+  height: 700px;
+`;
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  width: 500px;
+`;
+
+export const Heading = styled.h1`
+  color: yellow;
+  font-size: 32px;
+`;
+
+export const Text = styled.text`
+  color: white;
+  font-size: 18px;
+`;
