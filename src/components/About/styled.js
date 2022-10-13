e8769c8144d015b918610e84aed2348a64f5bf3f
Ram <ram.kodehode@gmail.com>
2022-10-13 11:41:46 +0200

    Added border and blur for text section

--- src/components/About/styled.js
+++ src/components/About/styled.js
@@ -25,4 +25,11 @@ export const Heading = styled.h1`
 export const Text = styled.text`
   color: white;
   font-size: 18px;
+  border: 1px yellow solid;
+  background: rgba(0, 0, 0, 0.63);
+  border-radius: 16px;
+  padding: 20px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(1.25px);
+  -webkit-backdrop-filter: blur(1.25px);
 `;
