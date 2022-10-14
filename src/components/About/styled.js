c296e8eb12e106dfc40c8001de6a78b93780aebe
Ram <ram.kodehode@gmail.com>
2022-10-14 13:31:40 +0200

    Making text container responsive

--- src/components/About/styled.js
+++ src/components/About/styled.js
@@ -1,36 +1,38 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Wrapper = styled.section`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 100%;
-    align-items: center;
-    height: 700px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  align-items: center;
+  height: 700px;
 `;
 
 export const Container = styled.header`
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-
-    width: 500px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 500px;
+  @media only screen and (max-width: 800px) {
+    width: 350px;
+  }
 `;
 
 export const Heading = styled.h1`
-    color: yellow;
-    font-size: 32px;
+  color: yellow;
+  font-size: 32px;
 `;
 
 export const Text = styled.p`
-    margin: 0;
-    color: white;
-    font-size: 18px;
-    border: 1px yellow solid;
-    background: rgba(0, 0, 0, 0.63);
-    border-radius: 16px;
-    padding: 20px;
-    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(1.25px);
-    -webkit-backdrop-filter: blur(1.25px);
+  margin: 0;
+  color: white;
+  font-size: 18px;
+  border: 1px yellow solid;
+  background: rgba(0, 0, 0, 0.63);
+  border-radius: 16px;
+  padding: 20px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(1.25px);
+  -webkit-backdrop-filter: blur(1.25px);
 `;
