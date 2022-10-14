2654469f94bccf4c5c9ac68e792e237b2485d9ba
stiankodehode <stiankodehode@gmail.com>
2022-10-14 13:21:26 +0200

    removed useless imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import LandingPage from "./pages/LandingPage";
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { BackgroundImage } from "./components/globalStyled";
 
 function App() {
